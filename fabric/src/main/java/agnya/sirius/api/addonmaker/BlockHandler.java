eee46a25b27bb7656eecfa68ef1185e412b349fc
nvb-uy <almhak@proton.me>
2022-11-01 13:21:36 -0600

    + AddonMaker blockreg handling

---
+++ fabric/src/main/java/agnya/sirius/api/addonmaker/BlockHandler.java
@@ -0,0 +1,19 @@
+package agnya.sirius.api.addonmaker;
+
+import agnya.sirius.api.registry.BRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item.Settings;
+
+public class BlockHandler {
+    public static void register(String[] lines, String MODID, Block block, Settings settings) {
+    /* Registers blocks from an array of lines. */
+    
+    for (int i = 0; i < lines.length; i++) {
+        if (lines[i] != "$comment") {
+            // Register block here.
+
+            BRegistry.regFullBlock(MODID, lines[i], block, settings);
+        }
+    }
+    }
+}
