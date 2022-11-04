46e551e58e9f271587104bdf910c0efbb099053f
nvb-uy <almhak@proton.me>
2022-11-03 18:14:08 -0600

    -+ Update BlockHandler comments

--- fabric/src/main/java/agnya/sirius/api/management/BlockHandler.java
+++ fabric/src/main/java/agnya/sirius/api/management/BlockHandler.java
@@ -6,14 +6,15 @@ import net.minecraft.item.Item.Settings;
 
 public class BlockHandler {
     public static void register(String[] lines, String MODID, Block block, Settings settings) {
-    /* Registers blocks from an array of lines. */
+    /* Registers blocks from an array of lines. 
+     *  This method should be in your block's class for automatic registration.
+     *  Parameters should be provided in the constructor of your class.
+    */
     
-    for (int i = 0; i < lines.length; i++) {
-        if (lines[i] != "$comment") {
-            // Register block here.
-
-            BRegistry.regFullBlock(MODID, lines[i], block, settings);
+        for (int i = 0; i < lines.length; i++) {
+            if (lines[i] != "$comment") {
+                BRegistry.regFullBlock(MODID, lines[i], block, settings);
+            }
         }
-    }
     }
 }
